415bec4cdca53db66284841facc7d6e708d73696
thezohaan <asifaly82@yahoo.com>
2014-11-05 00:55:25 +0300

    Removed update of First Letter and Lastword to Firebase Both are handled using Ng-Model and Ng-Bind

--- js/app.js
+++ js/app.js
@@ -3,21 +3,13 @@ var wordWire = angular.module('wordWire',['firebase']);
         wordWire.controller('WordCtrl', ['$scope', 'WordsService', '$firebase', 'FIREBASE_URI', function ($scope, WordsService, $firebase, firstlet, FIREBASE_URI) {
             //get last 10 words from firebase
             $scope.words = WordsService.getWords();
-            $scope.stats = WordsService.getStats();
-            $scope.errmsg = 'English please..no gibberish allowed here!';
             //initialize values
             $scope.newword = { name: '', score: ''};
-            $scope.wordpattern ="";
             
+            //add newword to firebase           
             $scope.addWord = function () {
-            //store the firstletter of lastword in the $scope
             WordsService.addWord(angular.copy($scope.newword));
-            
-            var a = $scope.newword.name;
-            var b = $scope.newword.name.charAt($scope.newword.name.length-1);
-            WordsService.addStats({lastword: a, firstletter: b});
-            //to store and get firstletter & lastword
-           
+                                   
             //clear the newword ng-model
             $scope.newword = { name: '', score: ''};
             };
@@ -25,8 +17,6 @@ var wordWire = angular.module('wordWire',['firebase']);
             //watch for changes to input field ng-model=newword and change newscore
             $scope.$watch("newword.name", function (newValue, oldValue){
             if ($scope.newword.name !== undefined) {
-            //$scope.wordpattern = firstlet($scope.words(4).name);
-            //console.log($scope.wordpattern);
             var wordy = $scope.newword.name;
             wordy = wordy.toUpperCase();
             scores = {'A':1,'B':3,'C':3,'D':2,'E':1,'F':4,'G':2,'H':4,'I':1,'J':8,'K':5,'L':1,'M':3, 'N':1,'O':1,'P':3,'Q':10,'R':1,'S':1,'T':1,'U':1,'V':4,'W':4,'X':8,'Y':4,'Z':10};
@@ -36,9 +26,7 @@ var wordWire = angular.module('wordWire',['firebase']);
 				sum += scores[wordy.charAt(i)] || 0;
 					}
 				$scope.newword.score = sum;
-                $scope.wordpattern=$scope.firstletter;
-                console.log($scope.wordpattern);
-			}
+            }
 			else {
 				//if value is undefined then it will set the score to zero
                 $scope.newword.score = 0;
@@ -49,39 +37,22 @@ var wordWire = angular.module('wordWire',['firebase']);
     wordWire.factory('WordsService', ['$firebase', 'FIREBASE_URI', function ($firebase, FIREBASE_URI) {
     var ref = new Firebase(FIREBASE_URI+"/words");
     var words = $firebase(ref.limit(5)).$asArray();
-    var stat = new Firebase(FIREBASE_URI+"/stats");
-    var stats = $firebase(stat.limit(1));
-    var last = new Firebase(FIREBASE_URI+"/stats/lastword");
-    var lasts = $firebase(last).$asObject();
-    var first = new Firebase(FIREBASE_URI+"/stats/firstletter");
-    var firsts = $firebase(first).$asObject();
-
+    
     var getWords = function () {
         return words;
     };
 
-    var getStats = function () {
-        return stats;
-    };
-
     var addWord = function (word) {
-        words.$add(word);
-    };
-
-    var addStats = function (stat) {
-        stats.$set(stat);
+    words.$add(word);
     };
 
     return {
         getWords: getWords,
-        getStats: getStats,
-        //getLast: getLast,
-        //getFirst: getFirst,
         addWord: addWord,
-        addStats: addStats,
         };
 }]);
 
+    //filter for score, it works but not sure how to use it in the model to upload to firebase
     wordWire.filter('score', function(){
         return function(text) {
         if (text !== undefined) {
@@ -100,7 +71,8 @@ var wordWire = angular.module('wordWire',['firebase']);
             };
     });
 
-        wordWire.filter('firstlet', function(){
+    //filter to catch the first letter of last word
+    wordWire.filter('firstlet', function(){
         return function(text) {
         if (text !== undefined) {
             text = text.charAt(text.length-1);
