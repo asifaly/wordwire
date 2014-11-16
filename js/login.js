006fd8a71851fa4b4d3a80bf1c6d880d22bb30da
thezohaan <asifaly82@yahoo.com>
2014-11-16 22:26:13 +0300

    - Initial Social Login Logic + Retrieve Display Name added

--- js/login.js
+++ js/login.js
@@ -1,42 +1,38 @@
-wordWire.controller('LoginCtrl', ['$scope', '$firebase', 'FIREBASE_URI', '$timeout', '$window', '$filter', function ($scope, $firebase, FIREBASE_URI, $timeout, $window, $filter) {
+wordWire.controller('LoginCtrl', ['$scope', '$firebase', '$window', 'FIREBASE_URI', function ($scope, $firebase, $window, FIREBASE_URI) {
     var uRef = new Firebase(FIREBASE_URI),
         usersRef = new Firebase(FIREBASE_URI + "users");
 
+
     $scope.user = {
         displayName: '',
-        uid: ''
+        uid: '',
+        provider: ''
     };
 
-    function logout() {
+    $scope.logout = function () {
         uRef.unauth();
         $scope.user = {
             displayName: '',
             uid: ''
         };
-    }
+    };
 
-    function so_google() {
+    $scope.google = function () {
         uRef.authWithOAuthPopup("google", function (err, authData) {
-            $scope.user.displayName = authData.google.displayName;
-            $scope.user.uid = authData.uid;
         });
-    }
+    };
 
-    function so_fb() {
+    $scope.facebook = function () {
         uRef.authWithOAuthPopup("facebook", function (err, authData) {
-            $scope.user.displayName = authData.facebook.displayName;
-            $scope.user.uid = authData.uid;
         });
-    }
+    };
 
-    function so_twitter() {
+    $scope.twitter = function () {
         uRef.authWithOAuthPopup("twitter", function (err, authData) {
-            $scope.user.displayName = authData.twitter.displayName;
-            $scope.user.uid = authData.uid;
         });
-    }
+    };
 
-    function so_passwd() {
+    $scope.email = function () {
         var email = $scope.user.email,
             password = $scope.user.password;
         uRef.createUser({
@@ -50,15 +46,41 @@ wordWire.controller('LoginCtrl', ['$scope', '$firebase', 'FIREBASE_URI', '$timeo
                 console.log("Error creating user:", error);
             }
         });
-    }
+    };
 
     uRef.onAuth(function (authData) {
         if (authData) {
+            setTimeout(function () {
+                $scope.$apply(function () {
+                    if (authData.provider === "google") {
+                        $scope.user = {
+                            displayName: authData.google.displayName,
+                            uid: authData.uid,
+                            provider: authData.provider
+                        };
+                    }
+                    else if (authData.provider === "facebook") {
+                        $scope.user = {
+                            displayName: authData.facebook.displayName,
+                            uid: authData.uid,
+                            provider: authData.provider
+                        };
+                    }
+                    else if (authData.provider === "twitter") {
+                        $scope.user = {
+                            displayName: authData.twitter.displayName,
+                            uid: authData.uid,
+                            provider: authData.provider
+                        };
+                    }
+                });
+            }, 100);
             // user authenticated with Firebase
             usersRef.child(authData.uid).once('value', function (snapshot) {
                 if (snapshot.val() !== null) {
-                    $window.alert("User Already Exists");
+                    //$window.alert("User Already Exists");
                     console.log(authData);
+                    //console.log(authData.google.displayName);
                 }
                 else {
                     uRef.child('users').child(authData.uid).set(authData);
